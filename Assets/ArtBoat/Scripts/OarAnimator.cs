5c9e5623559dee6889f8fb2eb5e136f043d339f3
Graham Pentheny <graham.pentheny@gmail.com>
2018-08-15 03:47:21 -0400

    Gameplay prototype
    Short straight river with rocks and input

---
+++ Assets/ArtBoat/Scripts/OarAnimator.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class OarAnimator : MonoBehaviour {
+    [SerializeField] Transform BackLeft;
+    [SerializeField] Transform FrontLeft;
+    
+    [SerializeField] Transform BackRight;
+    [SerializeField] Transform FrontRight;
+
+    [SerializeField] float backStrokeRotation = 30;
+    [SerializeField] float frontStrokeRotation = -30;
+
+    void Update() {
+        FrontLeft.localRotation = BackLeft.localRotation = Quaternion.Euler(
+            0, Input.GetKey(KeyCode.LeftArrow) ? frontStrokeRotation : backStrokeRotation, 0);
+        
+        FrontRight.localRotation = BackRight.localRotation = Quaternion.Euler(
+            0, Input.GetKey(KeyCode.RightArrow) ? -frontStrokeRotation : -backStrokeRotation, 0);
+    }
+}
