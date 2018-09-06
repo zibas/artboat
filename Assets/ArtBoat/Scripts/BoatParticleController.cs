b4e4407a74bb7be305b764f6c2721638349368df
ChrisMaire <christopherkmaire@gmail.com>
2018-09-05 23:54:09 -0400

    better particles

---
+++ Assets/ArtBoat/Scripts/BoatParticleController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoatParticleController : MonoBehaviour {
+    public ParticleSystem rippleParticles;
+    public Rigidbody body;
+    public float threshold = .5f;
+
+	void Update () {
+	    if(body.velocity.magnitude < threshold)
+        {
+            if(!rippleParticles.isEmitting)
+            {
+                rippleParticles.Play();
+            }
+        } else
+        {
+            if (rippleParticles.isEmitting)
+            {
+                rippleParticles.Pause();
+            }
+        }
+	}
+}
